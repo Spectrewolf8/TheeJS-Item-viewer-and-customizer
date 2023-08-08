fc43a23a721eb5e7758a0749adad4ee87106ebec
spectrewolf8 <salmantariq8385@gmail.com>
2023-08-08 12:55:11 +0500

    Added a working 3D model viewer with orbital controls

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-// import * as THREE from "../node_modules/three/build/three.module.js";
-// import WebGL from "../node_modules/three/examples/jsm/capabilities/WebGL.js";
-// import GLTFLoader from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
-
+console.log("main.js loaded successfully!");
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
-console.log("Importing GLTF loader");
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-console.log("main.js loaded successfully!");
+console.log("Imports successful!");
+
+//creating a scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(231, 231, 231);
+
+//creating rendered
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//creating main camera and adding orbital controls
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -17,42 +23,32 @@ const camera = new THREE.PerspectiveCamera(
   1000,
 );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+camera.position.z = 3;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//creating light sources
+const light0 = new THREE.PointLight(0xff0000, 500, 100);
+light0.position.set(-20, 0, 20);
+light0.rotateX = 45;
+scene.add(light0);
 
-camera.position.z = 5;
+const light1 = new THREE.PointLight(0xff0000, 200, 100);
+light1.position.set(+30, -10, -20);
+light1.rotateX = -45;
+scene.add(light1);
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-if (WebGL.isWebGLAvailable()) {
-  // Initiate function or other initializations here
-  animate();
-} else {
-  const warning = WebGL.getWebGLErrorMessage();
-  document.getElementById("container").appendChild(warning);
-}
+//loading 3D model and setting its attriutes
 console.log("loading 3D model");
 const loader = new GLTFLoader();
-// loader.load("../models/shoes_by_amor.glb", function (gltf) {
-//   scene.add(gltf.scene);
-// });
 
 loader.load(
-  "../models/shoes_by_amor.glb",
+  "../models/sneaker_shoe.glb",
   function (gltf) {
     const shoes = gltf.scene; // sword 3D object is loaded
-    shoes.scale.set(1, 1, 1);
+    shoes.scale.set(10, 10, 10);
     shoes.position.y = 0;
+    shoes.position.x = 0;
     scene.add(shoes);
   },
   undefined,
@@ -61,3 +57,19 @@ loader.load(
   },
 );
 console.log("3D model loaded");
+
+//setting things to animate in animate()
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+//checking browser's WebGL compatibility before animating
+if (WebGL.isWebGLAvailable()) {
+  animate();
+  console.log("WebGL support detected");
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById("container").appendChild(warning);
+}
