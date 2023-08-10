5ac7d7e99fedbe7eb3b4dbb6a7cd94a26c05392a
spectrewolf8 <salmantariq8385@gmail.com>
2023-08-10 22:20:01 +0500

    Added texture control

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 console.log("main.js loaded successfully!");
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
+import Stats from "three/addons/libs/stats.module.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
@@ -15,28 +16,34 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//stats
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
 //creating main camera and adding orbital controls
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000,
+  100,
 );
+camera.position.set(-2.75, 1.5, 1.5);
 
-camera.position.z = 3;
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0);
 controls.enableDamping = true;
-controls.dampingFactor = 1;
 controls.enableZoom = true;
 controls.enablePan = false;
+controls.minDistance = 2.5;
+controls.maxDistance = 4;
 controls.update();
 
 //creating helper grid
-const size = 100;
-const divisions = 50;
-const gridHelper = new THREE.GridHelper(size, divisions);
-gridHelper.position.y = -1;
-scene.add(gridHelper);
+// const size = 100;
+// const divisions = 50;
+// const gridHelper = new THREE.GridHelper(size, divisions);
+// gridHelper.position.y = -1;
+// scene.add(gridHelper);
 
 //creating light sources
 const light0 = new THREE.PointLight("#ffffff", 700, 100);
@@ -54,21 +61,27 @@ scene.add(light1);
 //loading 3D model and setting its attriutes
 console.log("loading 3D model");
 const loader = new GLTFLoader();
-
+let concerned_element;
 loader.load(
-  "../models/sneaker_shoe.glb",
+  "../models/sneaker_shoe_mod_3.glb",
   function (gltf) {
     const shoes = gltf.scene;
+    // console.log(shoes);
     // console.log(shoes.children.find((x) => x.name == "red"));
     var newMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-    shoes.traverse((element) => {
-      if (element.isMesh) {
-        console.log(element);
+    shoes.getObjectByName("sole_front_back___R_").material.color.set("#111111");
+    shoes.getObjectByName("sole_front_back___L_").material.color.set("#111111");
 
-        // if ((element.name == "R_SHOE_R_Shoe_0")) {
-        //   element.visible = false;
-        // }
-      }
+    shoes.traverse((element) => {
+      // element.material.color.set("#111111");
+      // if (element.name == "laces__L") {
+      //   // element.material = newMaterial;
+      //   element.material.color.set("#141414");
+      // }
+      console.log(element.name);
+      // if (element.isMaterial) {
+      //   element.material.color.set("#111111");
+      // }
     });
     shoes.scale.set(10, 10, 10);
     shoes.position.y = 0;
@@ -86,6 +99,7 @@ console.log("3D model loaded");
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
+  stats.update();
   renderer.render(scene, camera);
 }
 
