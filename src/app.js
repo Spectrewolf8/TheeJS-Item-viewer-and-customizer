5f230de4e6286e548a3465e4219bc86417ceeefd
spectrewolf8 <salmantariq8385@gmail.com>
2023-08-13 23:28:47 +0500

    Item Viewer completed. Color picker also added

--- src/app.js
+++ src/app.js
@@ -1,169 +1,3 @@
-// console.log("main.js loaded successfully!");
-// import * as THREE from "three";
-// import WebGL from "three/addons/capabilities/WebGL.js";
-// import Stats from "three/addons/libs/stats.module.js";
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
-// console.log("Imports successful!");
-// const intersectedObjects = [];
-
-// //creating a scene
-// const scene = new THREE.Scene();
-// scene.background = new THREE.Color("#141414");
-
-// //creating renderer
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// //stats
-// const stats = new Stats();
-// document.body.appendChild(stats.dom);
-
-// //creating main camera and adding orbital controls
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   100,
-// );
-// camera.position.set(-2.75, 1.5, 1.5);
-
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.target.set(0, 0.5, 0);
-// controls.enableDamping = true;
-// controls.enableZoom = true;
-// controls.enablePan = false;
-// controls.minDistance = 2.5;
-// controls.maxDistance = 4;
-// controls.update();
-
-// //creating helper grid
-// const size = 100;
-// const divisions = 50;
-// const gridHelper = new THREE.GridHelper(size, divisions);
-// gridHelper.position.y = -1;
-// scene.add(gridHelper);
-
-// //creating light sources
-// const light0 = new THREE.PointLight("#ffffff", 600, 100);
-// light0.position.set(-10, +10, +20);
-// light0.lookAt(0, 0, 0);
-// light0.rotateX = 45;
-// light0.rotateY = -15;
-// scene.add(light0);
-
-// const light1 = new THREE.PointLight("#ffffff", 300, 100);
-// light1.position.set(+30, -10, +20);
-// light1.lookAt(0, 0, 0);
-// light1.rotateX = -45;
-// light1.rotateY = 10;
-// scene.add(light1);
-
-// const light2 = new THREE.PointLight("#ffffff", 300, 100);
-// light2.position.set(-30, -10, -20);
-// light2.lookAt(0, 0, 0);
-// light2.rotateX = 180;
-// light2.rotateY = 10;
-// scene.add(light2);
-
-// const light3 = new THREE.PointLight("#ffffff", 300, 100);
-// light3.position.set(+30, -10, -20);
-// light3.lookAt(0, 0, 0);
-// light3.rotateX = 180;
-// light3.rotateY = 10;
-// scene.add(light3);
-
-// //loading 3D model and setting its attriutes
-// console.log("loading 3D model");
-// const loader = new GLTFLoader();
-// let concerned_element;
-// loader.load(
-//   "../models/sneaker_shoe_mod_3.glb",
-//   function (gltf) {
-//     const shoes = gltf.scene;
-//     concerned_element = shoes;
-//     console.log(shoes);
-//     // console.log(shoes.children.find((x) => x.name == "red"));
-//     var newMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-//     shoes.getObjectByName("L_SHOE_L_Shoe_0014").material.color.set("Cyan");
-//     shoes.getObjectByName("sole_front_back___L_").material.color.set("#111111");
-//     shoes.getObjectByName("L_SHOE_L_Shoe_0008").material.isMetal = false;
-//     shoes.getObjectByName("L_SHOE_L_Shoe_0014").material.isMetal = false;
-//     shoes.traverse((element) => {
-//       // element.material.color.set("#111111");
-//       // if (element.name == "laces__L") {
-//       //   // element.material = newMaterial;
-//       //   element.material.color.set("#141414");
-//       // }
-//       console.log(element.name);
-//       // if (element.isMaterial) {
-//       //   element.material.color.set("#111111");
-//       // }
-//     });
-//     shoes.scale.set(10, 10, 10);
-//     shoes.position.y = 0;
-//     shoes.position.x = 0;
-//     scene.add(shoes);
-//   },
-//   undefined,
-//   function (error) {
-//     console.error(error);
-//   },
-// );
-// console.log("3D model loaded");
-
-// //setting mouse event handler
-// function onMouseMove(event) {
-//   event.preventDefault();
-
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-// }
-
-// //setting up raycaster
-// const raycaster = new THREE.Raycaster();
-// const mouse = new THREE.Vector2(1, 1);
-
-// //setting things to animate in animate()
-// function animate() {
-//   requestAnimationFrame(animate);
-//   controls.update();
-//   stats.update();
-
-//   raycaster.setFromCamera(mouse, camera);
-//   const intersection = raycaster.intersectObject(concerned_element);
-//   console.log(intersection);
-//   renderer.render(scene, camera);
-// }
-
-// //checking browser's WebGL compatibility before animating
-// if (WebGL.isWebGLAvailable()) {
-//   requestAnimationFrame(animate);
-//   animate();
-//   console.log("WebGL support detected");
-// } else {
-//   const warning = WebGL.getWebGLErrorMessage();
-//   document.getElementById("container").appendChild(warning);
-// }
-
-// if (
-//   typeof intersection != undefined &&
-//   typeof intersection[0] != undefined
-// ) {
-//   console.log(intersection[0].object["name"]);
-//   concerned_element
-//     .getObjectByName(intersection[0].object["name"])
-//     .material.color.set("Blue");
-// }
-
-// // setTimeout(() => {
-// //   concerned_element
-// //     .getObjectByName("L_SHOE_L_Shoe_0014")
-// //     .material.color.set("Magenta");
-// // }, 5 * 1000);
-
 console.log("main.js loaded successfully!");
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
@@ -333,6 +167,14 @@ function init() {
 ​​
 9: Object { isObject3D: true, uuid: "7f495863-2bac-48bd-896c-db1c1462be87", name: "shoes_tag__L", … } */
 
+  const colorPicker = document.getElementById("colorPicker");
+  // = "#" + Math.floor(Math.random() * 16777215).toString(16)
+  let color = "#ffa1a1";
+  colorPicker.addEventListener("input", () => {
+    const selectedColor = colorPicker.value;
+    color = selectedColor;
+  });
+
   function animate() {
     requestAnimationFrame(animate);
     controls.update();
@@ -396,7 +238,6 @@ function init() {
 
         //handling actions on click
         if (mouseClicked && !isDragging) {
-          let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
           intersectedObject.material.color.set(color);
           console.log(color + " on " + intersectedObject.name);
           mouseClicked = false;
